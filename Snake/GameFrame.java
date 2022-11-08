7101e303e0c0a4da6d8338cded1ab11756770fcc
Jesse Johal <jesse.johal89@gmail.com>
2022-11-08 02:45:28 +0000

    commit dev branch

--- Snake/GameFrame.java
+++ Snake/GameFrame.java
@@ -3,5 +3,16 @@ package Snake;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
+	
+	GameFrame() {
+		
+		GamePanel panel = new GamePanel();
+		this.add(panel);
+		
+	}
+	
+	
+	
+	
 
 }
